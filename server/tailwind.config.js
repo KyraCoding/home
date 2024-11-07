41dcec424b290017435ee79a107a1515cf63e619
KyraCoding <pixelesc.com@gmail.com>
2024-11-07 11:44:58 -0500

    Add initial CSS, README, and configuration files for project setup

---
+++ server/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../client/public/*.{html,js,ejs}","../client/src/*.{html,js,ejs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
